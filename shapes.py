29cb2ab090930381dd6886d39318547599e25e51
Pavel Gaidukov <mailgpn@gmail.com>
2022-01-12 23:29:20 +0500

    All calculations in the parent class

--- shapes.py
+++ shapes.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
-from math import sqrt, sin, pi
-from typing import Tuple
+from math import radians, sqrt, tan
+
 
 @dataclass
 class Vertices:
@@ -18,33 +18,41 @@ class Vertices:
         """Compare vertices."""
         return self.x == other.x and self.y == other.y
 
-    def __str__(self) -> str:
-        return f'{self.x}, {self.y}'
+    def get_tuple(self) -> tuple:
+        return (self.x, self.y)
 
 
 class Shapes:
     """General class for shapes."""
 
-    def __init__(self, *vertices: Tuple[Vertices]) -> None:
+    NUM_SIDES = 0
+
+    def __init__(self, *vertices: Vertices) -> None:
         self.vertices = vertices
 
     def get_side(self) -> float:
-        """Get the side size."""
+        """Get the side size by the first two vertices."""
         return self.vertices[0].get_distance(self.vertices[1])
 
     def get_area(self) -> float:
         """Get the area of the shape."""
-        raise NotImplementedError(f'Define get_area in '
-                                  f'{self.__class__.__name__}')
+        if self.NUM_SIDES == 0:
+            raise NotImplementedError(f'Define NUM_SIDES in '
+                                      f'{self.__class__.__name__}')
+        return (((self.NUM_SIDES * (self.get_side() ** 2)) / 4)
+                * (1 / (tan(radians(180 / self.NUM_SIDES)))))
 
     def get_perimetr(self) -> float:
         """Get the perimeter of the shape."""
-        raise NotImplementedError(f'Define get_perimetr in '
-                                  f'{self.__class__.__name__}')
+        if self.NUM_SIDES == 0:
+            raise NotImplementedError(f'Define NUM_SIDES in '
+                                      f'{self.__class__.__name__}')
+        return self.get_side() * self.NUM_SIDES
 
     def __eq__(self, other) -> bool:
         """Compare shapes."""
-        return set(self.vertices) == set(other.vertices)
+        return (set(vert.get_tuple() for vert in self.vertices)
+                == set(vert.get_tuple() for vert in other.vertices))
 
     def get_info(self) -> str:
         """Return all values of shape"""
@@ -58,50 +66,24 @@ class Triangle(Shapes):
 
     NUM_SIDES = 3
 
-    def get_perimetr(self) -> float:
-        """Get the perimeter of the shape."""
-        return self.get_side() * self.NUM_SIDES
-
-    def get_area(self) -> float:
-        """Get the area of the shape."""
-        coefficient_area = sqrt(3) / 4
-        return coefficient_area * self.get_side() ** 2
-
 
 class Pentagon(Shapes):
 
     NUM_SIDES = 5
 
-    def get_perimetr(self) -> float:
-        """Get the perimeter of the shape."""
-        return self.get_side() * self.NUM_SIDES
-
-    def get_area(self) -> float:
-        """Get the area of the shape."""
-        side = self.get_side()
-        triangle_hip = side / (2 * sin(pi / 5))
-        triangle_area = 1/2 * side * sqrt(triangle_hip ** 2 - side ** 2 / 4)
-        return triangle_area * self.NUM_SIDES
-
 
 def main() -> None:
-    '''t = Triangle((2, 1), (6, 4), (4, 2))
-    print(t.get_info())
-
-    p = Pentagon((2, 1), (6, 4), (2, 1))
-    print(p.get_info())
-
-    t2 = Triangle((6, 4), (2, 1), (4, 2))
-    print(t == t2)'''
 
     vt1 = Vertices(6, 4)
     vt2 = Vertices(2, 1)
     vt3 = Vertices(4, 2)
     t = Triangle(vt1, vt2, vt3)
     t2 = Triangle(vt2, vt1, vt3)
+    p = Pentagon(vt1, vt2)
     print(t.get_info())
     print(t == t2)
     print(vt1)
+    print(p.get_info())
 
 
 if __name__ == '__main__':
